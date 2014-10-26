2f91fcca257a77f09af9d19ace92610b8e3487da
Vimal Manohar <vimal.manohar91@gmail.com>
2014-10-26 13:02:40 -0400

    kaldi-git/LatEnt: Adding scripts for NCE training of GMM models

--- egs/babel/s5b/local/run_ml_nce.sh
+++ egs/babel/s5b/local/run_ml_nce.sh
@@ -10,15 +10,16 @@ set -u
 num_iters=1
 train_stage=-10
 dir=dev10h.pem
+alpha=0.1 
 
 . utils/parse_options.sh
 
-if [ ! -f exp/tri5_nce/.done ]; then
-  steps/train_nce.sh --stage $train_stage --cmd "$train_cmd" \
-    --num-iters $num_iters \
-    data/unsup.uem data/lang exp/tri5/decode_unsup.uem \
-    exp/tri5_nce || exit 1
-  touch exp/tri5_nce/.done
+if [ ! -f exp/tri5_ml_nce/.done ]; then
+  steps/train_ml_nce.sh --stage $train_stage --cmd "$train_cmd" \
+    --num-iters $num_iters --alpha $alpha data/train \
+    data/unsup.uem data/lang exp/tri5_ali exp/tri5/decode_unsup.uem \
+    exp/tri5_ml_nce || exit 1
+  touch exp/tri5_ml_nce/.done
 fi
 
 dataset_dir=data/$dir
@@ -27,7 +28,7 @@ dataset_type=${dir%%.*}
 
 eval my_nj=\$${dataset_type}_nj  #for shadow, this will be re-set when appropriate
 
-decode=exp/tri5_nce/decode_${dataset_id}
+decode=exp/tri5_ml_nce/decode_${dataset_id}
 if [ ! -f ${decode}/.done ]; then
   mkdir -p $decode
   steps/decode.sh --skip-scoring true \
@@ -42,3 +43,6 @@ local/run_kws_stt_task.sh --cer $cer --max-states $max_states \
   --cmd "$decode_cmd" --skip-kws true --skip-stt false \
   "${lmwt_plp_extra_opts[@]}" \
   ${dataset_dir} data/lang ${decode}
+
+mkdir -p /home/vmanoha1/Results/LatEnt-Babel/
+grep $decode/score_*/*.ctm.sys | grep $dir >> /home/vmanoha1/Results/LatEnt-Babel/results-$(date +'%m_%d_%H_%M')
