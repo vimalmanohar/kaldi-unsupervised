2525c4696fcfba3ed1a4ff3beea40f893094c456
Vimal Manohar <vimal.manohar91@gmail.com>
2014-10-29 15:03:20 -0400

    kaldi-unsupervised/vimal: Added a test function to check lattice forward backward for computing entropy and derivatives

--- egs/babel/s5b/local/run_nce.sh
+++ egs/babel/s5b/local/run_nce.sh
@@ -7,27 +7,30 @@ set -e
 set -o pipefail
 set -u
 
-num_iters=1
+num_iters=10
 train_stage=-10
-dir=dev10h.pem
+alpha=1.0
+datadir=dev10h.pem
+dir=exp/tri5_nce
 
 . utils/parse_options.sh
+dir=${dir}_a${alpha}
 
-if [ ! -f exp/tri5_nce/.done ]; then
+if [ ! -f $dir/.done ]; then
   steps/train_nce.sh --stage $train_stage --cmd "$train_cmd" \
     --num-iters $num_iters \
     data/unsup.uem data/lang exp/tri5/decode_unsup.uem \
-    exp/tri5_nce || exit 1
-  touch exp/tri5_nce/.done
+    $dir || exit 1
+  touch $dir/.done
 fi
 
-dataset_dir=data/$dir
-dataset_id=$dir
-dataset_type=${dir%%.*}
+dataset_dir=data/$datadir
+dataset_id=$datadir
+dataset_type=${datadir%%.*}
 
 eval my_nj=\$${dataset_type}_nj  #for shadow, this will be re-set when appropriate
 
-decode=exp/tri5_nce/decode_${dataset_id}
+decode=$dir/decode_${dataset_id}
 if [ ! -f ${decode}/.done ]; then
   mkdir -p $decode
   steps/decode.sh --skip-scoring true \
@@ -42,3 +45,11 @@ local/run_kws_stt_task.sh --cer $cer --max-states $max_states \
   --cmd "$decode_cmd" --skip-kws true --skip-stt false \
   "${lmwt_plp_extra_opts[@]}" \
   ${dataset_dir} data/lang ${decode}
+
+mkdir -p /home/vmanoha1/Results/LatEnt-Babel/
+(
+date;
+grep "Sum" $decode/score_*/*.sys | grep $dir | utils/best_wer.sh 
+echo `pwd`/$dir
+) | tr '\n' ' ' >> /home/vmanoha1/Results/LatEnt-Babel/results
+
