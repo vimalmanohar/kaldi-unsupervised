2e8b7fe964dfb8ee09d3adba562c4486d93f8f6f
Vimal Manohar <vimal.manohar91@gmail.com>
2014-11-02 19:08:59 -0500

    kaldi-unsupervised/vimal: Modified unsupervised recipe for fisher english so that training of acoustic and language models can be done using different subsets. Also added programs to filter out lattices and transforms corresponding to only particular utt or spk

--- egs/fisher_english/s5/local/fisher_create_test_lang.sh
+++ egs/fisher_english/s5/local/fisher_create_test_lang.sh
@@ -3,13 +3,17 @@
 
 if [ -f path.sh ]; then . path.sh; fi
 
-mkdir -p data/lang_test
+lmdir=data/local/lm
+lang=data/lang_test
 
-arpa_lm=data/local/lm/3gram-mincount/lm_unpruned.gz
+. parse_options.sh
+
+mkdir -p $lang
+
+arpa_lm=$lmdir/3gram-mincount/lm_unpruned.gz
 [ ! -f $arpa_lm ] && echo No such file $arpa_lm && exit 1;
 
-mkdir -p data/lang_test
-cp -r data/lang/* data/lang_test
+cp -rT data/lang $lang
 
 # grep -v '<s> <s>' etc. is only for future-proofing this script.  Our
 # LM doesn't have these "invalid combinations".  These can cause 
@@ -23,14 +27,14 @@ gunzip -c "$arpa_lm" | \
    grep -v '</s> </s>' | \
    arpa2fst - | fstprint | \
    utils/remove_oovs.pl /dev/null | \
-   utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=data/lang_test/words.txt \
-     --osymbols=data/lang_test/words.txt  --keep_isymbols=false --keep_osymbols=false | \
-    fstrmepsilon | fstarcsort --sort_type=ilabel > data/lang_test/G.fst
-  fstisstochastic data/lang_test/G.fst
+   utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=$lang/words.txt \
+     --osymbols=$lang/words.txt  --keep_isymbols=false --keep_osymbols=false | \
+    fstrmepsilon | fstarcsort --sort_type=ilabel > $lang/G.fst
+  fstisstochastic $lang/G.fst
 
 
 echo  "Checking how stochastic G is (the first of these numbers should be small):"
-fstisstochastic data/lang_test/G.fst 
+fstisstochastic $lang/G.fst 
 
 ## Check lexicon.
 ## just have a look and make sure it seems sane.
@@ -40,21 +44,21 @@ fstprint   --isymbols=data/lang/phones.txt --osymbols=data/lang/words.txt data/l
 echo Performing further checks
 
 # Checking that G.fst is determinizable.
-fstdeterminize data/lang_test/G.fst /dev/null || echo Error determinizing G.
+fstdeterminize $lang/G.fst /dev/null || echo Error determinizing G.
 
 # Checking that L_disambig.fst is determinizable.
-fstdeterminize data/lang_test/L_disambig.fst /dev/null || echo Error determinizing L.
+fstdeterminize $lang/L_disambig.fst /dev/null || echo Error determinizing L.
 
 # Checking that disambiguated lexicon times G is determinizable
 # Note: we do this with fstdeterminizestar not fstdeterminize, as
 # fstdeterminize was taking forever (presumbaly relates to a bug
 # in this version of OpenFst that makes determinization slow for
 # some case).
-fsttablecompose data/lang_test/L_disambig.fst data/lang_test/G.fst | \
+fsttablecompose $lang/L_disambig.fst $lang/G.fst | \
    fstdeterminizestar >/dev/null || echo Error
 
 # Checking that LG is stochastic:
-fsttablecompose data/lang/L_disambig.fst data/lang_test/G.fst | \
+fsttablecompose data/lang/L_disambig.fst $lang/G.fst | \
    fstisstochastic || echo "[log:] LG is not stochastic"
 
 
