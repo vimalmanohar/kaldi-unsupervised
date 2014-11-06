339271909bc579fe6d8be1c03a9e7a5edfa778db
Vimal Manohar <vimal.manohar91@gmail.com>
2014-11-06 17:20:07 -0500

    kaldi-unsupervised: Added baseline SGMM scripts for fisher english

--- src/latbin/lattice-copy.cc
+++ src/latbin/lattice-copy.cc
@@ -24,6 +24,44 @@
 #include "fstext/fstext-lib.h"
 #include "lat/kaldi-lattice.h"
 
+int32 CopySubsetLattices(std::string filename, 
+                        SequentialLatticeReader *lattice_reader,
+                        LatticeWriter *lattice_writer,
+                        bool include = true, bool ignore_missing = false,
+                        ) {
+  unordered_set<std::string, StringHasher> subset;
+  bool binary;
+  Input ki(filename, binary);
+  KALDI_ASSERT(!binary);
+  std::string line;
+  while (std::getline(ki.Stream(), line)) {
+    std::vector<std::string> split_line;
+    SplitStringToVector(line, " \t\r", true, &split_line);
+    KALDI_ASSERT(!split_line.empty() &&
+        "Empty line encountered in input in " << filename);
+    subset.insert(split_line[0]);
+  }
+  
+  int32 num_total = 0;
+  size_t num_success = 0;
+  for (; !lattice_reader->Done(); lattice_reader->Next(), num_total++) {
+    if (include && subset.count(lattice_reader->Key()) > 0) {
+      lattice_writer->Write(lattice_reader->Key(), lattice_reader->Value());
+      num_success++;
+    } else if (!include && subset.count(lattice_reader->Key()) == 0) {
+      lattice_writer->Write(lattice_reader->Key(), lattice_reader->Value());
+      num_success++;
+    }
+  }
+
+  KALDI_LOG << " Wrote " << num_success << " out of " << num_total
+            << " utterances.";
+
+  if (ignore_missing) return 0;
+
+  return (num_success != 0 ? 0 : 1);
+}
+
 int main(int argc, char *argv[]) {
   try {
     using namespace kaldi;
@@ -36,14 +74,29 @@ int main(int argc, char *argv[]) {
     const char *usage =
         "Copy lattices (e.g. useful for changing to text mode or changing\n"
         "format to standard from compact lattice.)\n"
+        "The --include and --exclude mutually exclusive options of this "
+        "program, which are intended to copy only subset of lattices.\n"
         "Usage: lattice-copy [options] lattice-rspecifier lattice-wspecifier\n"
         " e.g.: lattice-copy --write-compact=false ark:1.lats ark,t:text.lats\n"
         "See also: lattice-to-fst, and the script egs/wsj/s5/utils/convert_slf.pl\n";
     
     ParseOptions po(usage);
-    bool write_compact = true;
+    bool write_compact = true, ignore_missing = false;
+    std::string include_rxfilename;
+    std::string exclude_rxfilename;
+
     po.Register("write-compact", &write_compact, "If true, write in normal (compact) form.");
-    
+    po.Register("include", &include_rxfilename, 
+                        "Text file, the first field of each "
+                        "line being interpreted as an "
+                        "utterance-id whose features will be included");
+    po.Register("exclude", &exclude_rxfilename, 
+                        "Text file, the first field of each "
+                        "line being interpreted as an utterance-id"
+                        " whose features will be excluded");
+    po.Register("ignore-missing", &ignore_missing,
+                        "Exit with status 0 even if no lattices are copied");
+
     po.Read(argc, argv);
 
     if (po.NumArgs() != 2) {
@@ -59,15 +112,44 @@ int main(int argc, char *argv[]) {
     if (write_compact) {
       SequentialCompactLatticeReader lattice_reader(lats_rspecifier);
       CompactLatticeWriter lattice_writer(lats_wspecifier);
+      
+      if (include_rxfilename != "") {
+        if (exclude_rxfilename != "") {
+          KALDI_ERR << "should not have both --exclude and --include option!";
+        }
+        return CopySubsetLattices(include_rxfilename,  
+            &lattice_reader, &lattice_writer,
+            true, ignore_missing);
+      } else if (exclude_rxfilename != "") {
+        return CopySubsetLattices(exclude_rxfilename, 
+            &lattice_reader, &lattice_writer,
+            false, ignore_missing);
+      }
+
       for (; !lattice_reader.Done(); lattice_reader.Next(), n_done++)
         lattice_writer.Write(lattice_reader.Key(), lattice_reader.Value());
     } else {
       SequentialLatticeReader lattice_reader(lats_rspecifier);
       LatticeWriter lattice_writer(lats_wspecifier);
+      
+      if (include_rxfilename != "") {
+        if (exclude_rxfilename != "") {
+          KALDI_ERR << "should not have both --exclude and --include option!";
+        }
+        return CopyIncludedLattices(include_rxfilename,
+            &lattice_reader, &lattice_writer);
+      } else if (exclude_rxfilename != "") {
+        return CopyExcludedLattices(exclude_rxfilename,
+            &lattice_reader, &lattice_writer);
+      }
+
       for (; !lattice_reader.Done(); lattice_reader.Next(), n_done++)
         lattice_writer.Write(lattice_reader.Key(), lattice_reader.Value());
     }
     KALDI_LOG << "Done copying " << n_done << " lattices.";
+    
+    if (ignore_missing) return 0;
+
     return (n_done != 0 ? 0 : 1);
   } catch(const std::exception &e) {
     std::cerr << e.what();
