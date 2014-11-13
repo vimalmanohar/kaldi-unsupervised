a916fbda94b7f1ca457c7edb185b8d93edc653eb
Vimal Manohar <vimal.manohar91@gmail.com>
2014-11-13 16:36:58 -0500

    kaldi-unsupervised/vimal: Added unsupervised NCE recipe combined with supervised MMI training. Also added baseline nnet2 training with 100 hrs subset of fisher_english

--- egs/fisher_english/s5/local/run_mmi_nce.sh
+++ egs/fisher_english/s5/local/run_mmi_nce.sh
@@ -2,10 +2,10 @@
 . cmd.sh
 
 num_iters=4
-tau=400
+tau=1600
 weight_tau=10
-alpha=1.0
-dir=exp/tri5a_nce_100k_250k
+alpha=10.0
+dir=exp/tri5a_mmi_nce_100k_250k
 do_decode=false
 stage=-8
 
@@ -16,17 +16,19 @@ echo "$0 $@"  # Print the command line for logging
 dir=${dir}_t${tau}_wt${weight_tau}_a${alpha}
 
 if [ ! -f $dir/.done ]; then 
-  steps/train_nce.sh --cmd "$train_cmd" --num-iters $num_iters \
+  steps/train_mmi_nce.sh --cmd "$train_cmd" --num-iters $num_iters \
     --tau $tau --weight-tau $weight_tau --alpha $alpha --stage $stage \
-    --transform-dir exp/tri4a/decode_100k_unsup_100k_250k \
-    data/unsup_100k_250k data/lang_100k_test exp/tri4a/decode_100k_unsup_100k_250k $dir || exit 1
+    --transform-dir-unsup exp/tri4a/decode_100k_unsup_100k_250k \
+    data/train_100k data/unsup_100k_250k data/lang_100k_test exp/tri4a \
+    exp/tri4a_denlats_100k exp/tri4a/decode_100k_unsup_100k_250k $dir || exit 1
   touch $dir/.done
 fi
 
 decode=$dir/decode_100k_dev_it${num_iters}
 if $do_decode  && [ ! -f $decode/.done ]; then
   steps/decode.sh --nj 25 --cmd "$decode_cmd" --config conf/decode.config \
-    --transform-dir exp/tri4a/decode_100k_dev --iter $num_iters \
+    --transform-dir exp/tri4a/decode_100k_dev \
     exp/tri4a/graph_100k data/dev $decode || exit 1
   touch $decode/.done
 fi
+
